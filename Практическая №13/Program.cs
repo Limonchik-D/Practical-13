634fe7588624b0509e4b9c4a281c2b50bc3a33e4
Limonchik-D <DavLimon4@yandex.com>
2024-11-27 21:35:49 +0200

    Add files via upload

---
+++ Практическая №13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая__13
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Задача 1. Найти минимальную величину из двух целых переменных a, b, 
+        /// используя вспомогательные методы (ввода аргументов, нахождения минимальной величины, вывода результата).
+        /// </summary>
+
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+
+
+        }
+        static int Max(int x, int y)
+        {
+            return x > y ? x : y;
+        }
+
+        static void Output(int max)
+        {
+            Console.WriteLine(max);
+        }
+
+        static void Main(string[] args)
+        {
+            int x = Input("x= ");
+            int y = Input("y= ");
+            int max = Max(x, y);
+            Console.WriteLine($"x = {max}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
